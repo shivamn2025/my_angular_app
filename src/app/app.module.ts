eccc371d0c52f6201f0781723dbe34f606b8d9eb
shivamn2025 <nanavareshivam639@gmail.com>
2025-06-29 11:29:40 +0530

    basic structure of angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomPipe } from './shared/custom.pipe';
+import { Dir1Directive } from './shared/dir1.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomPipe,
+    Dir1Directive
   ],
   imports: [
     BrowserModule,
